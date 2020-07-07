cee0834dcda1823923327a90905833e7487dabae
Arima Kaoru <46063788+arimakaoru@users.noreply.github.com>
2020-07-07 10:31:03 +0900

    Change: ディレクトリ構成を変更

--- app.h
+++ app.h
@@ -1,5 +1,6 @@
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include "ev3api.h"
@@ -12,10 +13,10 @@ extern "C" {
 
 #ifndef TOPPERS_MACRO_ONLY
 
-extern void main_task(intptr_t exinf);
-extern void bt_task(intptr_t exinf);
-extern void localization_task(intptr_t exinf);
-extern void color_task(intptr_t exinf);
+    extern void main_task(intptr_t exinf);
+    extern void bt_task(intptr_t exinf);
+    extern void localization_task(intptr_t exinf);
+    extern void color_task(intptr_t exinf);
 
 #endif /* TOPPERS_MACRO_ONLY */
 
