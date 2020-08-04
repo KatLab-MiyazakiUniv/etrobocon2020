c82fa8941cf8f531a8c339b8e9d9356c068016ba
Arima Kaoru <46063788+arimakaoru@users.noreply.github.com>
2020-08-04 22:08:33 +0900

    Change: トレッド幅をシミュレータ取扱説明書v2.0に従って変更

--- module/Curvature.h
+++ module/Curvature.h
@@ -10,20 +10,18 @@
 #include "Distance.h"
 #include "Pid.h"
 
-class Curvature
-{
+class Curvature {
  private:
   Pid pid;
   double targetCurvature;
   PidGain pidGain;
   double preDistance;
   double preTheta;
-  static constexpr double Tread = 128.0;
+  static constexpr double Tread = 140.0;
 
  public:
   Curvature(double targetCurvature, double Kp, double Ki, double Kd);
   double control(int leftAngle, int rightAngle, int velocity = 600);
-
 };
 
 #endif
