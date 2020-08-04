59576aa06a115295f94472635e36e2e8d2b444d6
Arima Kaoru <46063788+arimakaoru@users.noreply.github.com>
2020-08-04 23:26:55 +0900

    Update: 明るさの目標値を初期位置の光センサーの値とする

--- module/EtRobocon2020.cpp
+++ module/EtRobocon2020.cpp
@@ -15,10 +15,10 @@ void EtRobocon2020::start()
   // 白黒の基準値として実測値を設定
   constexpr rgb_raw_t standardWhite = { 7, 7, 12 };
   constexpr rgb_raw_t standardBlack = { 126, 120, 166 };
-  // 光センサーの目標値を設定
-  constexpr double targetBrightness = (255 - 0) / 2.0;
 
   Controller controller;
+  // 光センサーの目標値を設定
+  const double targetBrightness = controller.getBrightness();
   controller.setStandardWhite(standardWhite);
   controller.setStandardBlack(standardBlack);
 
