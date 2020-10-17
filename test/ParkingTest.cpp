6a33426b6ceea86a052400072a7bbff25be55302
Takahiro55555 <tkhr.corei7@gmail.com>
2020-10-18 00:09:17 +0900

    fix: フォーマッタを適用し、コメントの微修正を行った。

--- test/ParkingTest.cpp
+++ test/ParkingTest.cpp
@@ -8,26 +8,29 @@
 #include <gtest/gtest.h>
 
 namespace etrobocon2020_test {
-    TEST(Parking, construct) {
-        constexpr bool isLeftCourse = true;
-        constexpr int targetBrightness = (255 - 0) / 2;
-        Controller controller;
+  TEST(Parking, construct)
+  {
+    constexpr bool isLeftCourse = true;
+    constexpr int targetBrightness = (255 - 0) / 2;
+    Controller controller;
 
-        // インスタンス生成
-        Parking parking(isLeftCourse, controller, targetBrightness);
-    }
+    // インスタンス生成
+    Parking parking(isLeftCourse, controller, targetBrightness);
+  }
 
-    TEST(Parking, parkInGarage) {
-        constexpr bool isLeftCourse = true;
-        constexpr int targetBrightness = (255 - 0) / 2;
-        Controller controller;
+  TEST(Parking, parkInGarage)
+  {
+    constexpr bool isLeftCourse = true;
+    constexpr int targetBrightness = (255 - 0) / 2;
+    Controller controller;
 
-        // インスタンス生成
-        Parking parking(isLeftCourse, controller, targetBrightness);
-        
-        // 駐車
-        // NOTE: Linetracer.runTo???() や movestraight.moveTo() などがモックの Controller を使用するとき std::exti(1) を実行してしまう
-        // そのため、カバレッジが 100% にならない点に注意
-        ASSERT_EXIT(parking.parkInGarage(), ::testing::ExitedWithCode(1), "");
-    }
-}
+    // インスタンス生成
+    Parking parking(isLeftCourse, controller, targetBrightness);
+
+    // 駐車
+    // NOTE: Linetracer.runTo???() や movestraight.moveTo() などがモックの Controller
+    //      を使用するとき、 std::exti(1) を実行してしまう。
+    //      そのため、Parking.parkInGarage() のカバレッジが 100% にならない点に注意すること。
+    ASSERT_EXIT(parking.parkInGarage(), ::testing::ExitedWithCode(1), "");
+  }
+}  // namespace etrobocon2020_test
