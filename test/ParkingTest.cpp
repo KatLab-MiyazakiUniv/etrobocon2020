18ae72c18352cc839daf82a43444f5d605aa0b0d
Takahiro55555 <tkhr.corei7@gmail.com>
2020-10-18 00:50:26 +0900

    fix: 余計なテストコードの削除とコメントの微修正。

--- test/ParkingTest.cpp
+++ test/ParkingTest.cpp
@@ -8,16 +8,6 @@
 #include <gtest/gtest.h>
 
 namespace etrobocon2020_test {
-  TEST(Parking, construct)
-  {
-    constexpr bool isLeftCourse = true;
-    constexpr int targetBrightness = (255 - 0) / 2;
-    Controller controller;
-
-    // インスタンス生成
-    Parking parking(isLeftCourse, controller, targetBrightness);
-  }
-
   TEST(Parking, parkInGarage)
   {
     constexpr bool isLeftCourse = true;
@@ -28,9 +18,11 @@ namespace etrobocon2020_test {
     Parking parking(isLeftCourse, controller, targetBrightness);
 
     // 駐車
-    // NOTE: Linetracer.runTo???() や movestraight.moveTo() などがモックの Controller
-    //      を使用するとき、 std::exti(1) を実行してしまう。
-    //      そのため、Parking.parkInGarage() のカバレッジが 100% にならない点に注意すること。
+    // NOTE: Linetracer.runTo???() や Movestraight.moveTo() などをモックの Controller
+    //      使用して呼び出すとき、 Controler.tslpTsk() から std::exti(1) が実行される。
+    //      したがって、Linetracer.runTo???() や Movestraight.moveTo() が
+    //      呼ばれた部分以降のコードは実行されない。
+    //      そのため、Parking.parkInGarage()のカバレッジは 100% にならない点に注意すること。
     ASSERT_EXIT(parking.parkInGarage(), ::testing::ExitedWithCode(1), "");
   }
 }  // namespace etrobocon2020_test
